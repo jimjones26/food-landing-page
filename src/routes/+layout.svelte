1ee27333d6bf1f1241597acd1ed1923a7750fe83
jjones <jimmy.jones@tutanota.com>
2023-10-05 21:45:42 -0600

    add noise background and ripped edge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div class="relative w-screen h-screen">
-	<div class="flex w-full h-full">
-		<div
-			class="w-[58%] bg-no-repeat bg-cover bg-center bg-[#090909] bg-[url('grunge-overlay-5.png')]"
-		/>
-		<div class="bg-[#EABD67] flex-1" />
+	<div class="flex w-full h-full bg-[#090909]">
+		<div class="w-[58%] bg-no-repeat bg-cover bg-center bg-[url('grunge-overlay-5.png')]" />
+		<div class="flex-1 -ml-[47px]">
+			<span class="block top-[100%] h-full w-full left-0 bg-[url('rip.svg')] bg-cover" />
+		</div>
 	</div>
 	<div class="absolute top-0 right-0 bottom-0 left-0"><slot /></div>
 </div>
