e2d24362a84491722ce4054ee5f55decd8ec73cc
jjones <jimmy.jones@tutanota.com>
2023-10-05 21:04:02 -0600

    setup layout for two color background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
 	import '../app.postcss';
 </script>
 
-<slot />
+<div class="relative w-screen h-screen">
+	<div class="flex w-full h-full">
+		<div
+			class="w-[58%] bg-no-repeat bg-cover bg-center bg-[#090909] bg-[url('grunge-overlay-5.png')]"
+		/>
+		<div class="bg-[#EABD67] flex-1" />
+	</div>
+	<div class="absolute top-0 right-0 bottom-0 left-0"><slot /></div>
+</div>
