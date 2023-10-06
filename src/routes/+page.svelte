c884ff87ce4a738a554bdde62d7a6e54433404c9
jjones <jimmy.jones@tutanota.com>
2023-10-06 17:33:37 -0600

    refactor header into seperate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<header class="container flex py-20 px-20 items-center">
-	<div class="flex-1">
-		<img src="/logo.svg" alt="" class="max-w-[34px]" />
-	</div>
-	<nav class="flex space-x-10 text-sm text-black font-bold items-center">
-		<a href="/">Home</a>
-		<a href="/">Menu</a>
-		<a href="/">About Us</a>
-		<Button class="rounded-none px-8 h-9">Log In</Button>
-	</nav>
-</header>
+<Header />
