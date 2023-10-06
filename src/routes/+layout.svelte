5cbf440754ae5f133635b08386cc23c0accbfbf2
jjones <jimmy.jones@tutanota.com>
2023-10-06 16:27:16 -0600

    make background ui respoonsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,14 @@
 </script>
 
 <div class="relative w-screen h-screen">
-	<div class="flex w-full h-full bg-[#090909]">
-		<div class="w-[58%] bg-no-repeat bg-cover bg-center bg-[url('grunge-overlay-5.png')]" />
-		<div class="flex-1 -ml-[47px]">
-			<span class="block top-[100%] h-full w-full left-0 bg-[url('rip.svg')] bg-cover" />
+	<div class="flex flex-col lg:flex-row w-full h-full bg-[#090909]">
+		<div
+			class="h-[58%] lg:w-[58%] bg-no-repeat bg-cover bg-center bg-[url('grunge-overlay-5.png')]"
+		/>
+		<div class="flex-1 -mt-[47px] lg:-ml-[47px]">
+			<span
+				class="block left-[100%] lg:top-[100%] h-full w-full top-0 lg:left-0 bg-[url('rip-hor.svg')] lg:bg-[url('rip-vert.svg')] bg-cover"
+			/>
 		</div>
 	</div>
 	<div class="absolute top-0 right-0 bottom-0 left-0"><slot /></div>
