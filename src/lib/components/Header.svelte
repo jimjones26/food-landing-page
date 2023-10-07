d26d6317a9e0e6ddda9cfe5273f7a0fc7492d56c
jjones <jimmy.jones@tutanota.com>
2023-10-06 18:36:33 -0600

    update nav button styles

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,9 +7,19 @@
 		<img src="/logo.svg" alt="" class="max-w-[34px]" />
 	</div>
 	<nav class="flex items-center">
-		<Button variant="ghost" class="text-sm text-black font-bold px-5 h-8 rounded-none">Home</Button>
-		<Button variant="ghost" class="text-sm text-black font-bold px-5 h-8 rounded-none">Menu</Button>
-		<Button variant="ghost" class="text-sm text-black font-bold px-5 h-8 rounded-none"
+		<Button
+			variant="ghost"
+			class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67] active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
+			>Home</Button
+		>
+		<Button
+			variant="ghost"
+			class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67]  active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
+			>Menu</Button
+		>
+		<Button
+			variant="ghost"
+			class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67]  active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
 			>About Us</Button
 		>
 		<Button class="rounded-none px-8 h-8 ml-3">Log In</Button>
