f52adad3c63673b3f5416b46babf5c61268d6d47
jjones <jimmy.jones@tutanota.com>
2023-10-06 18:52:23 -0600

    hide menu itmes below sm breakpoint

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,21 +7,23 @@
 		<img src="/logo.svg" alt="" class="max-w-[34px]" />
 	</div>
 	<nav class="flex items-center">
-		<Button
-			variant="ghost"
-			class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67] active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
-			>Home</Button
-		>
-		<Button
-			variant="ghost"
-			class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67]  active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
-			>Menu</Button
-		>
-		<Button
-			variant="ghost"
-			class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67]  active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
-			>About Us</Button
-		>
+		<div class="hidden sm:flex">
+			<Button
+				variant="ghost"
+				class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67] active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
+				>Home</Button
+			>
+			<Button
+				variant="ghost"
+				class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67]  active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
+				>Menu</Button
+			>
+			<Button
+				variant="ghost"
+				class="text-[#EABD67] hover:bg-[#4F442F] hover:text-[#EABD67]  active:bg-black active:text-[#EABD67] lg:hover:bg-[#B08E4D] lg:active:text-black lg:active:bg-transparent lg:text-sm lg:text-black font-bold px-5 h-8 rounded-none lg:hover:text-black"
+				>About Us</Button
+			>
+		</div>
 		<Button
 			class="bg-[#EABD67] text-black font-bold hover:bg-[#4F442F] active:bg-[#EABD67] lg:bg-black lg:text-[#EABD67] lg:active:bg-black rounded-none px-8 h-8 ml-3"
 			>Log In</Button
